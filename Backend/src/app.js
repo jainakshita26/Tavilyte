2e949d004ee0a44057d12840e1fe20a806705820
jainakshita26 <jainakshita9886@gmail.com>
2026-06-14 16:29:07 +0530

    fix cors methods

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'))
 app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true,
-    methods:["GET",'POST','PUT','DELETE']
+    methods:["GET",'POST','PUT','DELETE','PATCH','OPTIONS']
 }))
 
 app.get('/',(req,res)=>{
