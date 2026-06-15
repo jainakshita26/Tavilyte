1cfbfe10e5a3892e63be612f520b9958c6b43713
jainakshita26 <jainakshita9886@gmail.com>
2026-06-15 09:47:20 +0530

    Fix:(fix cors origin)

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -15,7 +15,7 @@ app.use(morgan('dev'))
 app.use(cors({
     origin:process.env.CLIENT_URL,
     credentials:true,
-    methods:["GET",'POST','PUT','DELETE','PATCH','OPTIONS']
+    methods:'*'
 }))
 
 app.get('/',(req,res)=>{
