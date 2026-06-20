5f5df32e0948b5cf07e042c5233381559bc3624e
jainakshita26 <jainakshita9886@gmail.com>
2026-06-20 23:27:10 +0530

    Changes in UI

--- Backend/src/models/message.model.js
+++ Backend/src/models/message.model.js
@@ -5,7 +5,7 @@ const messageSchema = new mongoose.Schema(
         chat: {
             type: mongoose.Schema.Types.ObjectId,
             ref: 'Chat',
-            required: true,
+            required:true,
         },
         content: {
             type: String,
